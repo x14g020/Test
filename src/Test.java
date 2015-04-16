2e028b278ec6373ecfcfcd422d9b2f3102bb47de
x14g020 <x14g020@PC505N019.chiba-fjb.ac.jp>
2015-04-16 15:02:11 +0900

    a

---
+++ src/Test.java
@@ -0,0 +1,43 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+//		int a = 10;
+//		float b = 100.5f;
+//		double c = 1000.15;
+//		char d = 'M';
+//		String e = "ふぉ";
+//
+//		String s = String.format("整数:%d\n小数:%f\n小数:%f\n文字:%c\n文字列:%s\n",a,b,c,d,e);
+//		System.out.println(s);
+		
+//		String s = "AbCdE";
+//		//小文字
+//		System.out.println(s.toLowerCase());
+//		//大文字
+//		System.out.println(s.toUpperCase());
+		
+//		String s = "ABCDEFABCDEF";
+//		//CDをXYに置換
+//		String s2 = s.replaceAll("CD", "XY");
+//		System.out.println(s2);
+		
+//		 System.out.print("こんにちは世界\n");
+//	     System.out.print("こんにちは世界2\n");
+		
+//		int i;
+//		//iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+//		for(i=0;i<3;i=i+1)  
+//		    System.out.println(i);  //iの値を出力し改行
+		
+		int[] a = new int[3];
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+
+		System.out.println(a.length);
+
+	}
+
+}
